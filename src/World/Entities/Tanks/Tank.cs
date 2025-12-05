12acde38cfc679a5b7462c9d9225788c16711af9
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-12-05 15:17:26 -0400

    Added basic Tank class with NIE for abstract method.

---
+++ src/World/Entities/Tanks/Tank.cs
@@ -0,0 +1,9 @@
+namespace ZapTank.World.Entities.Tanks;
+
+public partial class Tank:TankBase
+{
+    protected override void Fire()
+    {
+        throw new System.NotImplementedException();
+    }
+}
