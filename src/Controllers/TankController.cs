7133d1568be59c37a2183e6dcb5c1c33496f4d5d
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-12-06 11:21:08 -0400

    Restructured tank controller

--- src/Controllers/TankController.cs
+++ src/Controllers/TankController.cs
@@ -4,25 +4,18 @@ using ZapTank.World.Entities.Tanks;
 namespace ZapTank.Controllers;
 
 /// <summary>
-/// Responsible for controlling a given tank.
+/// Responsible for providing inputs to a given tank. Records the last input state of the given tank.
 /// </summary>
 public abstract partial class TankController:Node
 {
+    public Vector2 LookDirection { get; protected set; }
+    public Vector2 MoveDirection { get; protected set; }
+    public bool FireInput { get; protected set; }
+
     [Export] protected TankBase Parent;
 
     /// <summary>
-    /// Updates velocity every physics tick.
+    /// Updates the given input fields when an input action is provided.
     /// </summary>
-    protected abstract void UpdateVelocity();
-    
-    /// <summary>
-    /// Updates rotation every physics tick.
-    /// </summary>
-    protected abstract void UpdateRotation();
-
-    public override void _PhysicsProcess(double delta)
-    {
-        UpdateRotation();
-        UpdateVelocity();
-    }
+    protected abstract void UpdateInputStates(InputEvent input);
 }
