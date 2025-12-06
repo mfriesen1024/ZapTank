002adf8572057338920aa8b0d2f0b2dd0547f339
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-12-06 11:41:16 -0400

    Added constants for input action names.

---
+++ src/Core/DataManager.cs
@@ -0,0 +1,23 @@
+namespace ZapTank.Core;
+
+/// <summary>
+/// Responsible for data handling, and exposes constant values for anything that needs to be hardcoded.
+/// </summary>
+public class DataManager
+{
+    #region InputConstants
+
+    public const string InputMoveLeft = "MoveLeft";
+    public const string InputMoveRight = "MoveRight";
+    public const string InputMoveUp = "MoveUp";
+    public const string InputMoveDown = "MoveDown";
+    
+    public const string InputLookLeft = "LookLeft";
+    public const string InputLookRight = "LookRight";
+    public const string InputLookUp = "LookUp";
+    public const string InputLookDown = "LookDown";
+    
+    public const string InputFire = "Fire";
+    
+    #endregion
+}
